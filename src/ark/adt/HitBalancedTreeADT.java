78a323ae3d58b9ae6e4ba3393d0e43b1703eeaad
arnaskro <arnas.kromelis@via.dk>
2015-09-17 15:54:58 +0200

    Node and exceptions

---
+++ src/ark/adt/HitBalancedTreeADT.java
@@ -0,0 +1,13 @@
+/**
+ * HitBalancedTreeADT defines the interface to a hit balanced tree data structure.
+ *
+ * @author Arnas Kromelis
+ * @version 1.0, 9/17/15
+ */
+
+package ark.adt;
+
+public interface HitBalancedTreeADT<T> {
+
+
+}
